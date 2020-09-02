ac88c3dc0abb663830d23c8cc0771f473b31fdce
Pauline Momot <pauline.momot@gmail.com>
2020-09-02 11:26:33 +0200

    Ajout des articles fonctionnel

--- index.js
+++ index.js
@@ -14,35 +14,44 @@ request.onload = function() {
     console.log(cameras);
 
 
-//ALGORYTHME :
+//ALGORITHME :
 // POUR CHAQUE élément du tableau 
 // ON CREE une card
 // et ON REMPLI les champs avec ce qui correspond
 
 
 //CREATION DES CARDS POUR AFFICHER LES DIFFERENTES CAMERAS
-// CREATION D'UNE BOUCLE
-for (let i = 0; i < cameras.length; i++){
+//CREATION D'UNE BOUCLE
 
-    function cardCreation (cameras){
+
+    function cardCreation (){
+
+// Creation de la div parente parente row
+        let divParentParent = document.createElement("div");
+        const main = document.getElementById("main");
+        main.appendChild(divParentParent);
+        divParentParent.classList.add("row-cols-1", "row-cols-md-4","row-cols-lg-5","d-flex","flex-wrap","justify-content-between","align-items-between");
+
+for (let i = 0; i < cameras.length; i++){
 
 // Création de la div parente
 // Ajout des classes 
+
     let divParent = document.createElement("div");
-    const main = document.getElementById("main");
-    main.appendChild(divParent);
-    divParent.classList.add("card","col","col-4-lg","m-3","pt-3");
+    divParentParent.appendChild(divParent);
+    divParent.classList.add("card","col","m-3","pt-3");
 
 
 // Création des élements images et div avec la classe card body, enfants de la divParent 
 // Ajout des classes
     let imageCamera = document.createElement("img");
     divParent.appendChild(imageCamera);
-    imageCamera.classList.add("card-image-top","photo");
+    imageCamera.classList.add("card-image-top","photo","img-fluid");
+    imageCamera.src = cameras[i].imageUrl;
 
     let divCardBody = document.createElement("div");
     divParent.appendChild(divCardBody);
-    divCardBody.classList.add("card-body");
+    divCardBody.classList.add("card-body", "text-center", "px-0", "d-flex","flex-column","justify-content-between");
 
 
 // Création des éléments enfants de la div CardBody
@@ -55,29 +64,34 @@ for (let i = 0; i < cameras.length; i++){
 
     let descriptionCamera = document.createElement("p");
     divCardBody.appendChild(descriptionCamera);
-    descriptionCamera.classList.add("description");
+    descriptionCamera.classList.add("description","text-justify");
     descriptionCamera.textContent = cameras[i].description;
 
-    let priceCamera = document.createElement("p");
-    divCardBody.appendChild(priceCamera);
-    priceCamera.classList.add("price");
-    pricenCamera.textContent = cameras[i].price;
 
+// Création d'une div englobant prix et bouton
+    let divLinkPrice = document.createElement("div");
+    divCardBody.appendChild(divLinkPrice);
+    divLinkPrice.classList.add("d-flex", "flex-row","justify-content-between");
+
+// Création du prix
+    let priceCamera = document.createElement("p");
+    divLinkPrice.appendChild(priceCamera);
+    priceCamera.classList.add("price", "my-2", "font-weight-bold");
+    priceCamera.textContent = cameras[i].price + ' $';
+
+    
     // Lien vers la page produit
     let linkProduct = document.createElement("a");
-    divCardBody.appendChild(linkProduct);
+    divLinkPrice.appendChild(linkProduct);
+    linkProduct.href="product.html";
         // Création du bouton 
         let buttonBuy = document.createElement("button");
         linkProduct.appendChild(buttonBuy);
-        buttonBuy.classList.add("btn", "btn-warning");
+        buttonBuy.classList.add("btn", "btn-warning", "block-right");
         // Ajout texte au bouton
         buttonBuy.textContent = "Voir";
 }
 }
-}
-
 // Appel de la fonction
-// cardCreation();
-
-// // BOUCLE CREER UNE CARD POUR CHAQUE I DU TABLEAU DE L API
-// for (let i = 0, i < cameraArray.lenghts, )}
+cardCreation();
+}
